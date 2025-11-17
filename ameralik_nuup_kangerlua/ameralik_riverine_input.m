a1132debe6a15ee4b644f673930a4ea4c466c68b
AnnekeV <anneke-vries@hotmail.com>
2025-11-17 09:07:27 +0000

    added 'real' FW input to river example

--- ameralik_nuup_kangerlua/ameralik_riverine_input.m
+++ ameralik_nuup_kangerlua/ameralik_riverine_input.m
@@ -11,49 +11,51 @@ path2sourcecode = '~/OneDrive - University of Edinburgh/fjordMIX/code/fjordrpm/'
 addpath(genpath(path2sourcecode));
 
 % get basic constants and default controlling parameters
-p = default_parameters;
+p = parameters_ameralik;
 p.Kb = 1e-5; % vertical mixing
 
 % can adjust any of the default parameters afterwards if needed
 % p.C0 = 5e4; % for example adjust shelf exchange parameter
 % p.run_plume_every = 10; % or update plume model only every 10 time steps
 
-% set up fjord geometry
-p.W = 6e3; % fjord width (m)
-p.L = 60e3; % fjord length (m)
-p.H = 800; % fjord depth (m)
-p.sill = 1; % p.sill=1 for presence of sill, p.sill=0 for no sill
-p.Hsill = 200; % sill depth below surface (m), only used if p.sill=1
-
-% set up glacier geometry
-% (only used if there is non-zero subglacial discharge)
-p.Hgl = 800; % grounding line depth (m)
-p.Wp = 250; % subglacial discharge plume width (m)
-
-% in the case of multiple plumes, either at the same glacier or at
-% different glaciers, specify vectors of grounding line depth and plume
-% width. for example, 3 glaciers of grounding line depth 800, 700, 600 m
-% and plume width 300, 200 and 300 m would require
-% p.Hgl = [800,700,600];
-% p.Wp = [300,200,300];
 
 % set up model layers
 p.N = 80; % number of layers
 a.H0 = (p.H/p.N)*ones(p.N,1); % layer thicknesses, here taken to be equal
 
+
+
+% Read CSV into table
+% monthly data, so 12 data points
+T_GIC_15_19 = readtable('/Users/annek/Library/CloudStorage/OneDrive-SharedLibraries-NIOZ/PhD Anneke Vries - General/fjord_modelling_ameralik/data/processed/racmo_ameralik_gic_runoff_2015_2020.csv');
+T_GrIS_15_19 = readtable('/Users/annek/Library/CloudStorage/OneDrive-SharedLibraries-NIOZ/PhD Anneke Vries - General/fjord_modelling_ameralik/data/processed/racmo_ameralik_gris_runoff_2015_2020.csv');
+T_Tundra_15_19 = readtable('/Users/annek/Library/CloudStorage/OneDrive-SharedLibraries-NIOZ/PhD Anneke Vries - General/fjord_modelling_ameralik/data/processed/racmo_ameralik_tundra_runoff_2015_2020.csv');
+
+T_GIC =    T_GIC_15_19(year(T_GIC_15_19.time) == 2019, :);  % select 2019
+T_GrIS =   T_GrIS_15_19(year(T_GrIS_15_19.time) == 2019, :);  % select 2019
+T_Tundra = T_Tundra_15_19(year(T_Tundra_15_19.time) == 2019, :);  % select 2019
+
+
+
 % set up time stepping
 dt = 0.2; % time step (in days)
-t_end = 3*365; % time to end the simulation (in days)
-t = 0:dt:t_end; % resulting time vector for simulation
+% set up surface forcing - surface freshwater input
+f.tsurf = datenum(T_GIC.time).';
+nt = numel(f.tsurf);
+assert(isequal(size(f.tsurf), [1 nt]), 'f.tsurf must be 1 x nt');
+t_start = f.tsurf(1);
+t_end = f.tsurf(end); % time to end the simulation (in days)
+t = t_start:dt:t_end; % resulting time vector for simulation
 p.t_save = 0:1:t_end; % times on which to save output
 
-% set up surface forcing - surface freshwater input
-% here use idealised seasonal gaussian peaked at julian day 200
-f.tsurf = t; % time vector for surface forcing
-f.Qr = 300*exp(-((mod(f.tsurf,365)-200)/30).^2); % riverine input on ta
-f.Tr = 0*t; % temperature of riverine input
-f.Sr = 0*t; % salinity of riverine input
-f.Ta = 0*t; % air temperature
+% Assign runoff column
+f.Qr = (T_GIC.runoff_m3_s + T_GrIS.runoff_m3_s + T_Tundra.runoff_m3_s).'; % riverine input on ta
+f.tsurf = f.tsurf;   % transpose time vector for surface forcing
+
+
+f.Tr = 0*f.tsurf; % temperature of riverine input
+f.Sr = 0*f.tsurf; % salinity of riverine input
+f.Ta = 0*f.tsurf; % air temperature
 p.kairsea = 0; % to turn off surface heat fluxes
 
 % set up shelf forcing - here constant in time and depth
@@ -62,6 +64,7 @@ p.kairsea = 0; % to turn off surface heat fluxes
 % f.zs must have dimensions nz x 1
 % f.Ss and f.Ts must have dimensions nz x nt
 f.ts = [0,t_end]; % time vector for shelf forcing
+f.ts = f.tsurf;
 f.zs = [-p.H;0]; % depth vector for shelf forcing (negative below surface)
 f.Ss = 34*ones(length(f.zs),length(f.ts)); % shelf salinity on (zs,ts)
 f.Ts = 3*ones(length(f.zs),length(f.ts)); % shelf temperature on (zs,ts)
@@ -84,10 +87,10 @@ a.I0 = 0*a.H0;
 s = run_model(p, t, f, a);
 
 % save the output
-save example5_riverine_input.mat s p t f a
+save ameralik_riverine_input.mat s p t f a
 
 % make an animation of the output (takes a few minutes)
-animate(p,s,50,'example5_riverine_input');
+% animate(p,s,50,'ameralik_riverine_input');
 
 % make basic plots of the output
 plotrpm(p,s,50);
